a43c68895b69a5745ae9082446bcd341fd58d4f5
Nicolas Chamorro <nichagiro@gmail.com>
2024-08-22 16:01:45 -0500

    add new number and remove repeat

--- components/custom/PhoneFloat.jsx
+++ components/custom/PhoneFloat.jsx
@@ -1,52 +1,36 @@
+import { CELL_PHONES, PHONES } from 'global/contact'
 import React from 'react'
 
 const PhoneFloat = () => {
-    return (
-        <div className="dropend">
-            <button className=" d-flex call align-items-center justify-content-center wow bounce" type="button"
-                id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i className="fas fa-phone-alt fa-2x"></i>
-            </button>
-            <ul className="dropdown-menu dropdown-menu-dark mx-2" aria-labelledby="dropdownMenuButton">
-                <li className="text-center">
-                    <a href="tel:6024396831" className="text-white">
-                        <i className="fa fa-phone-square"></i>
-                        &nbsp; 439 6831
-                    </a>
-                </li>
-                <li className="text-center">
-                    <a href="tel:6025244494" className="text-white">
-                        <i className="fa fa-phone-square"></i>
-                        &nbsp; 524 4494
-                    </a>
-                </li>
-                <li className="text-center">
-                    <a href="tel:6024393880" className="text-white">
-                        <i className="fa fa-phone-square"></i>
-                        &nbsp;439 3880
-                    </a>
-                </li>
-                <li className="text-center">
-                    <a href="tel:3105334254" className="text-white">
-                        <i className="fa fa-phone-square"></i>
-                        &nbsp;310 533 4254
-                    </a>
-                </li>
-                <li className="text-center">
-                    <a href="tel:3105334254" className="text-white">
-                        <i className="fa fa-phone-square"></i>
-                        &nbsp; 310 533 4254
-                    </a>
-                </li>
-                <li className="text-center">
-                    <a href="tel:3155220780 " className="text-white">
-                        <i className="fa fa-phone-square"></i>
-                        &nbsp; 315 522 0780
-                    </a>
-                </li>
-            </ul>
-        </div>
-    )
+	return (
+		<div className="dropend">
+			<button className=" d-flex call align-items-center justify-content-center wow bounce" type="button"
+				id="contactMenuPhone" data-bs-toggle="dropdown" aria-expanded="false">
+				<i className="fas fa-phone-alt fa-2x"></i>
+			</button>
+			<ul className="dropdown-menu dropdown-menu-dark mx-2" aria-labelledby="contactMenuPhone">
+				{
+					PHONES.map(number => (
+						<li className="text-center" key={number}>
+							<a href={`tel:602${number}`} className="text-white">
+								<i className="fa fa-phone-square" /> {number}
+							</a>
+						</li>
+					))
+				}
+				{
+					CELL_PHONES.map(number => (
+						<li className="text-center" key={number}>
+							<a href={`tel:${number}`} className="text-white">
+								<i className="fa fa-phone-square" /> {number}
+							</a>
+						</li>
+					))
+				}
+
+			</ul>
+		</div>
+	)
 }
 
 export default PhoneFloat
--- containers/home/Form.jsx
+++ containers/home/Form.jsx
@@ -1,113 +1,118 @@
 import React, { useRef } from 'react'
 
 const Form = () => {
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = async event => {
-        event.preventDefault();
-        const salto = '%0A'
-        const number = '573105334254'
-        const data = new FormData(form.current);
-        const name = data.get('name')
-        const phone = data.get('phone')
-        const subject = data.get('subject')
-        const email = data.get('email')
-        const message = data.get('message')
-        const textFormatter = `*Formulario Trasteos Gomez Gomez*
+  const sendEmail = async event => {
+    event.preventDefault();
+    const salto = '%0A'
+    const number = '573105334254'
+    const data = new FormData(form.current);
+    const name = data.get('name')
+    const phone = data.get('phone')
+    const subject = data.get('subject')
+    const email = data.get('email')
+    const message = data.get('message')
+    const textFormatter = `*Formulario Trasteos Gomez Gomez*
             ${salto}${salto}*Nombre:* ${name} 
             ${salto}*Telefono:* ${phone} 
             ${salto}*Asunto:* ${subject} 
             ${salto}*Email:* ${email} 
             ${salto}*Mensaje:* ${message}`
 
-        window.open(`https://wa.me/${number}?text=${textFormatter}`)
-        form.current.reset()
-    }
+    window.open(`https://wa.me/${number}?text=${textFormatter}`)
+    form.current.reset()
+  }
 
-    return (
-        <>
-            <div className="row" id="contact">
-                <div className="col-md-12">
-                    <div className="section-title">
-                        <h5>COTIZACIONES Y PREGUNTAS.</h5>
-                        <h2>
-                            FORMULARIO DE CONTACTO
-                        </h2>
-                    </div>
-                </div>
+  return (
+    <div className='bg-gray section-padding'>
+      <div className='container'>
+        <div className="row" id="contact">
+          <div className="col-md-12">
+            <div className="section-title">
+              <h5>COTIZACIONES Y PREGUNTAS.</h5>
+              <h2>
+                FORMULARIO DE CONTACTO
+              </h2>
             </div>
-            <div className="container-fluid shadow-lg wow fadeInUp" style={{ visibility: 'visible', animationName: 'fadeInUp' }}>
-                <div className="row text-white flex-column-reverse flex-lg-row">
-                    <div className="col-12 col-lg-6 p-4 p-sm-5 p-md-5 text-md-left">
-                        <form ref={form} onSubmit={sendEmail}>
-                            <div className="row">
-                                <div className="col">
-                                    <input type="text"
-                                        name="name"
-                                        className="form-control"
-                                        placeholder="Nombre*"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input type="text"
-                                        name="email"
-                                        className="form-control"
-                                        placeholder="Email*"
-                                    />
-                                </div>
-                            </div>
-                            <div className="row my-4">
-                                <div className="col">
-                                    <input type="text"
-                                        name="phone"
-                                        className="form-control"
-                                        placeholder="Telefono"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input type="text"
-                                        name="subject"
-                                        className="form-control"
-                                        placeholder="Asunto"
-                                    />
-                                </div>
-                            </div>
-                            <div className="row my-4">
-                                <div className="col">
-                                    <textarea
-                                        name="message"
-                                        className="form-control"
-                                        style={{ resize: "none" }}
-                                        rows="5"
-                                        placeholder="Mensaje*"
-                                    />
-                                </div>
-                            </div>
-                            <div className="row justify-content-center">
-                                <button
-                                    type="submit"
-                                    className="button home-btn-1 js-scroll"
-                                >
-                                    Enviar
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="col-12 col-lg-6 d-flex">
-                        <picture>
-                            <source srcSet="/assets/img/8.webp" />
-                            <img
-                                loading="lazy"
-                                className="img-fluid"
-                                src="/assets/img/8.webp"
-                                alt="trasteos gomez gomez"
-                            />
-                        </picture>
-                    </div>
+          </div>
+        </div>
+        <div className="container-fluid shadow-lg wow fadeInUp" style={{ visibility: 'visible', animationName: 'fadeInUp' }}>
+          <div className="row text-white flex-column-reverse flex-lg-row">
+            <div className="col-12 col-lg-6 p-4 p-sm-5 p-md-5 text-md-left">
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="row">
+                  <div className="col">
+                    <input type="text"
+                      name="name"
+                      className="form-control"
+                      placeholder="Nombre*"
+                      required
+                    />
+                  </div>
+                  <div className="col">
+                    <input type="text"
+                      name="email"
+                      className="form-control"
+                      placeholder="Email*"
+                      required
+                    />
+                  </div>
                 </div>
+                <div className="row my-4">
+                  <div className="col">
+                    <input type="text"
+                      name="phone"
+                      className="form-control"
+                      placeholder="Telefono"
+                    />
+                  </div>
+                  <div className="col">
+                    <input type="text"
+                      name="subject"
+                      className="form-control"
+                      placeholder="Asunto"
+                    />
+                  </div>
+                </div>
+                <div className="row my-4">
+                  <div className="col">
+                    <textarea
+                      name="message"
+                      className="form-control"
+                      style={{ resize: "none" }}
+                      rows="5"
+                      placeholder="Mensaje*"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <button
+                    type="submit"
+                    className="button home-btn-1 js-scroll"
+                  >
+                    Enviar
+                  </button>
+                </div>
+              </form>
             </div>
-        </>
-    )
+            <div className="col-12 col-lg-6 d-flex">
+              <picture>
+                <source srcSet="/assets/img/8.webp" />
+                <img
+                  loading="lazy"
+                  className="img-fluid"
+                  src="/assets/img/8.webp"
+                  alt="trasteos gomez gomez"
+                />
+              </picture>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Form
